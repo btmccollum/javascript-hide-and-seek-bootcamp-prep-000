b1430426ee25dedbe42f22b526eb4f05d10a20b8
Brad McCollum <btmcco8@gmail.com>
2018-06-13 02:53:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function nestedTarget() {
   return document.querySelector('#nested .target');
 }
 function increaseRankBy(n) {
-  var ranks = document.querySelectorAll('ul.ranked-list li');
-  for (var i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
+  var rankings = document.querySelectorAll('ul.ranked-list li');
+  for (var i = 0; i < rankings.length; i++) {
+    rankings[i].innerHTML = parseInt(rankings[i].innerHTML) + n;
   }
 }
 function deepestChild() {
